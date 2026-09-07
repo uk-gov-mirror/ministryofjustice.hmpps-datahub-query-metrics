6cf1b5f2b1ca757d10dda50299307e446f18af3d
James Reed <james.reed1@justice.gov.uk>
2026-09-07 14:19:37 +0100

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,18 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.7"
   kotlin("plugin.spring") version "2.4.10"
 }
 
+val awsVersion = "1.8.44"
+
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:3.0.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:3.0.1")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-webclient")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.3")
-  implementation("aws.sdk.kotlin:athena:1.8.25")
-  implementation("aws.sdk.kotlin:redshiftdata:1.8.25")
-  implementation("aws.sdk.kotlin:sts:1.8.25")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.1.0")
+  implementation("aws.sdk.kotlin:athena:$awsVersion")
+  implementation("aws.sdk.kotlin:redshiftdata:$awsVersion")
+  implementation("aws.sdk.kotlin:sts:$awsVersion")
   implementation("io.prometheus:prometheus-metrics-core")
   implementation("io.prometheus:prometheus-metrics-exporter-httpserver")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
@@ -18,11 +20,11 @@ dependencies {
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.11.0")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test-jvm:1.11.0")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:3.0.0")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:3.0.1")
   testImplementation("org.springframework.boot:spring-boot-starter-webflux-test")
   testImplementation("org.wiremock:wiremock-standalone:3.13.2")
   testImplementation("org.springframework.boot:spring-boot-test-autoconfigure")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.44") {
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.48") {
     exclude(group = "io.swagger.core.v3")
   }
 }
